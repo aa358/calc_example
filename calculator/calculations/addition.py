13d75bbb3367e46394f616c11c15129e78bd94e3
Arqam Usman Ali <aa358@njit.edu>
2021-11-15 21:57:41 -0500

    Update addition.py

--- calculator/calculations/addition.py
+++ calculator/calculations/addition.py
@@ -1,5 +1,5 @@
 """Addition Class"""
-from calc.calculations.calculation import Calculation
+from calculator.calculations.calculation import Calculation
 
 class Addition(Calculation):
     """ calculation addition class"""
