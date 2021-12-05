4556c4a5ebc8beda7ec944f382e8f377cb312097
Arqam Usman Ali <aa358@njit.edu>
2021-12-05 16:23:26 -0500

    Update test_addition.py

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,11 +1,11 @@
-"""Testing Addition"""
-from calculator.calculations.addition import Addition
+"""Testing addition"""
+from calc.operations.addition import Addition
 
-def test_calculation_addition():
-    """testing that our calculator has a static method for addition"""
-    #Arrange
-    nums = (3.0,2.0)
+
+# pylint: disable=too-few-public-methods
+
+def test_addition():
+    """testing calc result"""
+    nums = (3.0, 2.0)
     addition = Addition(nums)
-    #Act
-    #Assert
     assert addition.get_result() == 5.0
