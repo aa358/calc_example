6f4e31c02774da0344f81007a5373094c16acfee
Arqam Usman Ali <aa358@njit.edu>
2021-12-05 16:26:28 -0500

    Update test_multiply.py

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -1,11 +1,19 @@
-"""Testing Multiplication"""
-from calculator.calculations.multiplication import Multiplication
+"""Testing multiplication"""
+from calc.operations.multiplication import Multiplication
 
-def test_calculation_subtraction():
-    """testing that our calculator has a static method for multiplication"""
-    #Arrange
-    nums = (4.0,2.0)
-    multiplication = Multiplication(nums)
-    #Act
-    #Assert
-    assert multiplication.get_result() == 8.0
+
+# pylint: disable=too-few-public-methods
+
+
+def test_multiply():
+    """testing 3 multiplied by 2"""
+    nums = (3.0, 2.0)
+    multiply = Multiplication(nums)
+    assert multiply.get_result() == 6.0
+
+
+def test_multiply_incorrect():
+    """testing 3 multiplied by 2"""
+    nums = (3.0, 3.0)
+    multiply = Multiplication(nums)
+    assert multiply.get_result() != 6.0
